ae5e1a2bd6af63cc4eb64e73524fed53772c7790
Muhammad Faiz Asy Syauqi <m.faizasysyauqi@gmail.com>
2024-11-13 17:12:15 +0700

    feat(styles): add new color, text styles in colors.dart, text_style.dart

---
+++ lib/core/styles/colors.dart
@@ -0,0 +1,12 @@
+// Color
+
+import 'package:flutter/material.dart';
+
+const Color transparentColor = Colors.transparent;
+
+Color? primaryColor = const Color(0xFF121212);
+const Color secondaryColor = Colors.yellowAccent;
+Color? tertiaryColor = const Color(0xFF282828);
+Color? quaternaryColor = Colors.black;
+const Color quinaryColor = Colors.green;
+const Color senaryColor = Colors.grey;
