9021b13362d8a0309891421c1d26789c5dd1ddd0
Muhammad Faiz Asy Syauqi <m.faizasysyauqi@gmail.com>
2024-11-14 16:11:42 +0700

    update: modify app_constants in constants folder

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,6 @@
+// lib/core/constants/app_constants.dart
+class LayoutBreakpoints {
+  static const double tablet = 800.0;
+  static const double desktop = 1300.0;
+  static const double maxSecondaryContainerWidth = 370.0;
+}
